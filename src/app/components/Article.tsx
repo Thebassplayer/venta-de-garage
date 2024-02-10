66d330573854f6d86d7e44394a688b8186e498eb
Roy Lopez <roylopezdev@gmail.com>
2024-02-10 17:51:48 -0300

    Remove unused ArticleProps type definition

--- src/app/components/Article.tsx
+++ src/app/components/Article.tsx
@@ -1,20 +1,4 @@
-export type ArticleProps = {
-  titulo: string | null | undefined;
-  marca: string | null | undefined;
-  modelo: string | null | undefined;
-  detalles: string | null | undefined;
-  precio: string | null | undefined;
-  imagenes: string | string[] | null | undefined;
-  id?: string | null | undefined;
-  slug?: string | null | undefined;
-  comentarios: string | null | undefined;
-  linkMercadoLibre?: string | null | undefined;
-  linkFacebook?: string | null | undefined;
-  vendido?: boolean | null | undefined;
-  valorNuevo?: string | null | undefined;
-};
-
-const Article = () => {
+const Article = (article: UseGoogleSheetData) => {
   return <div>Article</div>;
 };
 
