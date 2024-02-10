2bc13c157d756563bafb9ff33767d8c039db4bbd
Roy Lopez <roylopezdev@gmail.com>
2024-02-10 17:51:31 -0300

    Add Card component to display table data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import useGoogleSheetData from "./hooks/UseGoogleSheetData";
+import Card from "./components/Card";
 
 export default function Home() {
   const { loading, error, tableTitles, tableData } = useGoogleSheetData();
@@ -16,16 +17,11 @@ export default function Home() {
   return (
     <main className="">
       <h1>Google Sheet Data</h1>
-      {tableData.map((row, index) => (
-        <div key={index} className="grid grid-cols-2 border border-red-400">
-          {tableTitles.map((title, titleIndex) => (
-            <div key={titleIndex} className="">
-              <span className="text-lg bg-slate-600">{title}: </span>
-              <span>{row[title]}</span>
-            </div>
-          ))}
-        </div>
-      ))}
+      <div className="grid grid-cols-4 gap-4">
+        {tableData.map((article, index) => (
+          <Card key={index} article={article} />
+        ))}
+      </div>
     </main>
   );
 }
