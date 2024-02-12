f5809ef571e92503269f67a5aaec2f3af2f430ed
Roy Lopez <roylopezdev@gmail.com>
2024-02-12 19:29:07 -0300

    Add Google Sheets API route and update data fetch URL

--- src/app/hooks/UseGoogleSheetData.tsx
+++ src/app/hooks/UseGoogleSheetData.tsx
@@ -25,7 +25,7 @@ const useGoogleSheetData = (): UseGoogleSheetData => {
   useEffect(() => {
     const fetchData = async () => {
       const getData = async (): Promise<ApiResponse> => {
-        const res = await fetch("/api/sheets");
+        const res = await fetch("/api/googlesheet");
         if (!res.ok) {
           throw new Error("Failed to fetch data");
         }
