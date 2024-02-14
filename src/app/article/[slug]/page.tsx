5164554e278c834eb6aa8c875204e3133350b570
Roy Lopez <roylopezdev@gmail.com>
2024-02-13 21:17:54 -0300

    Refactor image rendering in Article component

--- src/app/article/[slug]/page.tsx
+++ src/app/article/[slug]/page.tsx
@@ -49,40 +49,34 @@ const Article = ({ params }: { params: { slug: string } }): JSX.Element => {
   return (
     <main className="m-4 rounded-sm border border-black">
       <h1 className="text-center text-2xl py-3">{titulo || ""}</h1>
-      <div className="px-10 w-full object-cover h-1/2 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-        {[imagen1, imagen2, imagen3].map((imagen, index) => {
-          if (!imagen || imagen === "") {
-            return (
-              <div
-                key={index}
-                className="relative flex justify-center items-center"
-              >
+      <div className="px-10 w-full h-1/2">
+        <div className="relative grid grid-cols-2 lg:grid-cols-3 gap-4">
+          {[imagen1, imagen2, imagen3].map((imagen, index) => {
+            if (!imagen || imagen === "") {
+              return (
                 <CldImage
+                  key={index}
                   height={300}
                   width={300}
                   src="https://res.cloudinary.com/dukkbmkvk/image/upload/c_scale,q_48,w_367/v1707601780/venta_garage/e9f01hh4kutrdbsen9bs.jpg"
                   alt={titulo}
                   crop="fill_pad"
                 />
-              </div>
-            );
-          }
+              );
+            }
 
-          return (
-            <div
-              key={index}
-              className="relative flex justify-center items-center"
-            >
+            return (
               <CldImage
+                key={index}
                 height={300}
                 width={300}
                 src={imagen}
                 alt={titulo}
                 crop="fill_pad"
               />
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
       <div className="w-full p-4">
         <p>
