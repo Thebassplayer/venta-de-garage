09a33bb5849e29bb32a69e8366bcd8a4f2cff50c
Roy Lopez <roylopezdev@gmail.com>
2024-02-14 22:03:34 -0300

    Update article page title styling

--- src/app/article/[slug]/page.tsx
+++ src/app/article/[slug]/page.tsx
@@ -56,7 +56,7 @@ const Article = ({ params }: { params: { slug: string } }): JSX.Element => {
     <main className="w-screen md:h-[100svh] p-4 min-h-svh">
       <div className="rounded-sm border border-black h-full flex flex-col relative">
         <HomeButton />
-        <h1 className="text-center font-bold text-2xl md:text-4xl py-8 ocean_gradient">
+        <h1 className="text-center font-bold text-4xl pb-8 pt-16 md:py-6 ocean_gradient">
           {titulo || ""}
         </h1>
 
