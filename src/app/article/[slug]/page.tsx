50afbefbd9f1b44106e9347d857358d85f34f74d
Roy Lopez <roylopezdev@gmail.com>
2024-02-14 21:04:14 -0300

    Refactor article page layout and add conditional rendering for sold items

--- src/app/article/[slug]/page.tsx
+++ src/app/article/[slug]/page.tsx
@@ -43,69 +43,75 @@ const Article = ({ params }: { params: { slug: string } }): JSX.Element => {
 
   if (vendido) {
     return (
-      <main className="m-4 rounded-sm border border-black">
-        <h1 className="text-center text-2xl py-3">Artículo vendido</h1>
+      <main className="w-screen h-screen p-4">
+        <div className="rounded-sm border border-black h-full flex justify-center items-center">
+          <h1 className="text-center text-2xl py-3 text-red-600">
+            Artículo vendido
+          </h1>
+        </div>
       </main>
     );
   }
 
   return (
-    <main className="m-4 rounded-sm border border-black">
-      <h1 className="text-center text-2xl py-3">{titulo || ""}</h1>
+    <main className="w-screen h-screen p-4">
+      <div className="rounded-sm border border-black h-full">
+        <h1 className="text-center text-2xl py-3">{titulo || ""}</h1>
 
-      <div className="relative grid grid-cols-2 lg:grid-cols-3 gap-4 px-10 w-full h-1/2">
-        {[imagen1, imagen2, imagen3].map((imagen, index) => {
-          if (!imagen || imagen === "") {
+        <div className="relative grid grid-cols-2 lg:grid-cols-3 gap-4 px-10 w-full h-1/2">
+          {[imagen1, imagen2, imagen3].map((imagen, index) => {
+            if (!imagen || imagen === "") {
+              return (
+                <CldImage
+                  className="m-auto"
+                  key={index}
+                  height={300}
+                  width={300}
+                  src="https://res.cloudinary.com/dukkbmkvk/image/upload/c_scale,q_48,w_367/v1707601780/venta_garage/e9f01hh4kutrdbsen9bs.jpg"
+                  alt={titulo}
+                  crop="fill_pad"
+                />
+              );
+            }
+
             return (
               <CldImage
                 className="m-auto"
                 key={index}
                 height={300}
                 width={300}
-                src="https://res.cloudinary.com/dukkbmkvk/image/upload/c_scale,q_48,w_367/v1707601780/venta_garage/e9f01hh4kutrdbsen9bs.jpg"
+                src={imagen}
                 alt={titulo}
                 crop="fill_pad"
               />
             );
-          }
+          })}
+        </div>
 
-          return (
-            <CldImage
-              className="m-auto"
-              key={index}
-              height={300}
-              width={300}
-              src={imagen}
-              alt={titulo}
-              crop="fill_pad"
-            />
-          );
-        })}
-      </div>
-
-      <div className="w-full p-4">
-        <p>
-          <span className="underline-offset-2 underline">Marca: </span>
-          <span>{marca}</span>
-        </p>
-        <p>
-          <span className="underline-offset-2 underline">Modelo: </span>
-          <span>{modelo}</span>
-        </p>
-        <p>
-          <span className="underline-offset-2 underline">Detalle: </span>
-          <span>{detalles}</span>
-        </p>
-        <p>
-          <span className="underline-offset-2 underline">Precio: </span>
-          <span>{precio}</span>
-        </p>
-      </div>
-      <div className="w-full flex justify-center my-4">
-        <WhatsAppButton
-          phoneNumber="541133449591"
-          message={`Estoy interesad@ en ${titulo}`}
-        />
+        <div className="w-full p-4">
+          <p>
+            <span className="underline-offset-2 underline">Marca: </span>
+            <span>{marca}</span>
+          </p>
+          <p>
+            <span className="underline-offset-2 underline">Modelo: </span>
+            <span>{modelo}</span>
+          </p>
+          <p>
+            <span className="underline-offset-2 underline">Detalle: </span>
+            <span>{detalles}</span>
+          </p>
+          <p>
+            <span className="underline-offset-2 underline">Precio: </span>
+            <span>{precio}</span>
+          </p>
+        </div>
+        <div className="w-full flex justify-center my-4">
+          <WhatsAppButton
+            phoneNumber="541133449591"
+            message={`Estoy interesad@ en ${titulo}`}
+          />
+        </div>
       </div>
     </main>
   );
