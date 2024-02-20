f33f5c41acee67eeae77f073a44c392bd022956a
Roy Lopez <roylopezdev@gmail.com>
2024-02-20 15:49:13 -0300

    Add Footer component to main page

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="w-full bg-black p-3 text-center text-sm text-white dark:bg-white dark:text-black">
+      R3 - 2024 - Todos los derechos reservados
+    </footer>
+  );
+};
+
+export default Footer;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Card from "./components/Card";
 import CompartirPorWhatsAppButton from "./components/CompartirPorWhatsappButton";
 import WhatsAppButton from "./components/WhatsAppButton";
 import InfoButton from "./components/InfoButton";
+import Footer from "./components/Footer";
 
 const whatsappMessage = "Hola! estoy interesad@ en este artículo:";
 const whattsappShareMessage =
@@ -34,26 +35,29 @@ export default function Home() {
   }
 
   return (
-    <main className="p-3">
-      <h1 className="py-4 text-center text-2xl font-bold">Venta de Garage</h1>
-      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        {tableData.map((article, index) => {
-          if (article.vendido === "TRUE") return;
-          return <Card key={index} article={article as any} />;
-        })}
+    <main>
+      <div className="px-3 pb-3">
+        <h1 className="py-4 text-center text-3xl font-bold">Venta de Garage</h1>
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          {tableData.map((article, index) => {
+            if (article.vendido === "TRUE") return;
+            return <Card key={index} article={article as any} />;
+          })}
+        </div>
+        <div className="fixed bottom-4 right-2 z-50 flex justify-between gap-4 px-4">
+          <InfoButton />
+          <WhatsAppButton
+            buttonVersion="compact"
+            phoneNumber="541133449591"
+            message={`${whatsappMessage}`}
+          />
+          <CompartirPorWhatsAppButton
+            buttonVersion="compact"
+            message={whattsappShareMessage}
+          />
+        </div>
       </div>
-      <div className="fixed bottom-4 right-2 z-50 flex justify-between gap-4 px-4">
-        <InfoButton />
-        <WhatsAppButton
-          buttonVersion="compact"
-          phoneNumber="541133449591"
-          message={`${whatsappMessage}`}
-        />
-        <CompartirPorWhatsAppButton
-          buttonVersion="compact"
-          message={whattsappShareMessage}
-        />
-      </div>
+      <Footer />
     </main>
   );
 }
