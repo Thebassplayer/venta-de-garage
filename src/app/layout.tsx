d488c2aa5d5fb7d1f67ed48d8a4588ce6eb2191d
Roy Lopez <roylopezdev@gmail.com>
2024-02-19 23:21:35 -0300

    Add Suspense to InfoModal component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import InfoModal from "./components/InfoModal";
+import { Suspense } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +20,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         {children}
-        <InfoModal />
+        <Suspense fallback={<>Loading...</>}>
+          <InfoModal />
+        </Suspense>
       </body>
     </html>
   );
