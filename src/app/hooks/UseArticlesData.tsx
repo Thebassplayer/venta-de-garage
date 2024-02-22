6be6432573f3deecb6ca0bca8bc7fa5e11068b2d
Roy Lopez <roylopezdev@gmail.com>
2024-02-22 10:28:49 -0300

    update useArticleData

---
+++ src/app/hooks/UseArticlesData.tsx
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react";
+
+export type UseGoogleSheetData = {
+  loading: boolean;
+  error: string | null;
+  tableTitles: string[];
+  tableData: Record<string, string>[];
+};
+
+const useArticlesData = (): UseGoogleSheetData => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+  const [tableTitles, setTableTitles] = useState<string[]>([]);
+  const [tableData, setTableData] = useState<Record<string, string>[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch("/api/articles");
+        if (!res.ok) {
+          throw new Error("Failed to fetch data");
+        }
+
+        const data = await res.json();
+
+        setTableTitles(data.tableTitles);
+        setTableData(data.tableData);
+      } catch (error: any) {
+        setError(error.message || "Failed to fetch data");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return { loading, error, tableTitles, tableData };
+};
+
+export default useArticlesData;
