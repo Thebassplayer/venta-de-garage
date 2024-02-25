4fdc28ab120c861b5fab3a10d98a6792a2f8c419
Roy Lopez <roylopezdev@gmail.com>
2024-02-24 22:09:08 -0300

    Add tooltips to share and WhatsApp buttons

--- src/app/components/CompartirPorWhatsappButton.tsx
+++ src/app/components/CompartirPorWhatsappButton.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShare } from "@fortawesome/free-solid-svg-icons";
 import { cx } from "@/app/utils/styles";
+import Tooltip from "./Tooltip";
 
 type CompartirPorWhatsAppButtonProps = {
   className?: string;
@@ -20,30 +21,34 @@ const CompartirPorWhatsAppButton = ({
   };
   if (buttonVersion === "compact") {
     return (
+      <Tooltip message="Compartir por Whatsapp">
+        <button
+          rel="noopener noreferrer"
+          className={cx(
+            `flex h-14 w-14 cursor-pointer items-center justify-center rounded-full border border-black bg-orange-500 p-3 text-white transition-all duration-200 hover:scale-125 hover:bg-orange-600 lg:p-6 lg:text-base`,
+            className,
+          )}
+          onClick={handleClick}
+        >
+          <FontAwesomeIcon icon={faShare} size="xl" />
+        </button>
+      </Tooltip>
+    );
+  }
+  return (
+    <Tooltip message="Compartir por Whatsapp">
       <button
         rel="noopener noreferrer"
         className={cx(
-          `flex h-14 w-14 cursor-pointer items-center justify-center rounded-full border border-black bg-orange-500 p-3 text-white transition-all duration-200 hover:scale-125 hover:bg-orange-600 lg:p-6 lg:text-base`,
+          `flex w-40 cursor-pointer items-center justify-center rounded-full border border-black bg-orange-500 px-2 py-3 text-sm font-bold text-white transition-all duration-200 hover:scale-125 hover:bg-orange-600 lg:px-6 lg:py-4 lg:text-base`,
           className,
         )}
         onClick={handleClick}
       >
-        <FontAwesomeIcon icon={faShare} size="xl" />
+        <FontAwesomeIcon icon={faShare} size="xl" className="mr-2" />
+        <p>Compartir</p>
       </button>
-    );
-  }
-  return (
-    <button
-      rel="noopener noreferrer"
-      className={cx(
-        `flex w-40 cursor-pointer items-center justify-center rounded-full border border-black bg-orange-500 px-2 py-3 text-sm font-bold text-white transition-all duration-200 hover:scale-125 hover:bg-orange-600 lg:px-6 lg:py-4 lg:text-base`,
-        className,
-      )}
-      onClick={handleClick}
-    >
-      <FontAwesomeIcon icon={faShare} size="xl" className="mr-2" />
-      <p>Compartir</p>
-    </button>
+    </Tooltip>
   );
 };
 
--- src/app/components/WhatsAppButton.tsx
+++ src/app/components/WhatsAppButton.tsx
@@ -1,6 +1,7 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
 import { cx } from "@/app/utils/styles";
+import Tooltip from "./Tooltip";
 
 export type WhatsAppButtonProps = {
   phoneNumber: string;
@@ -23,31 +24,35 @@ const WhatsAppButton = ({
   };
   if (buttonVersion === "compact") {
     return (
+      <Tooltip message="Consultas por Whatsapp">
+        <button
+          rel="noopener noreferrer"
+          className={cx(
+            `flex h-14 w-14 cursor-pointer items-center justify-center rounded-full border border-black bg-green-500 p-3 text-white transition-all duration-200 hover:scale-125 hover:bg-green-600 lg:p-6 lg:text-base`,
+            className,
+          )}
+          onClick={handleClick}
+        >
+          <FontAwesomeIcon icon={faWhatsapp} size="xl" />
+        </button>
+      </Tooltip>
+    );
+  }
+  return (
+    <Tooltip message="Consultas por Whatsapp">
       <button
         rel="noopener noreferrer"
         className={cx(
-          `flex h-14 w-14 cursor-pointer items-center justify-center rounded-full border border-black bg-green-500 p-3 text-white transition-all duration-200 hover:scale-125 hover:bg-green-600 lg:p-6 lg:text-base`,
+          `flex w-40 cursor-pointer items-center justify-center rounded-full border
+       border-black bg-green-500 px-2 py-3 text-sm font-bold text-white transition-all duration-200 hover:scale-125 hover:bg-green-600 lg:px-6 lg:py-4 lg:text-base`,
           className,
         )}
         onClick={handleClick}
       >
-        <FontAwesomeIcon icon={faWhatsapp} size="xl" />
+        <FontAwesomeIcon icon={faWhatsapp} size="xl" className="mr-2" />
+        <p>Whatsapp</p>
       </button>
-    );
-  }
-  return (
-    <button
-      rel="noopener noreferrer"
-      className={cx(
-        `flex w-40 cursor-pointer items-center justify-center rounded-full border
-       border-black bg-green-500 px-2 py-3 text-sm font-bold text-white transition-all duration-200 hover:scale-125 hover:bg-green-600 lg:px-6 lg:py-4 lg:text-base`,
-        className,
-      )}
-      onClick={handleClick}
-    >
-      <FontAwesomeIcon icon={faWhatsapp} size="xl" className="mr-2" />
-      <p>Whatsapp</p>
-    </button>
+    </Tooltip>
   );
 };
 
