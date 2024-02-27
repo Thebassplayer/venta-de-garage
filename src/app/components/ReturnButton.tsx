bf897c173ae106ad00f8f4d5e6ea3d2bd9abe7b5
Roy Lopez <roylopezdev@gmail.com>
2024-02-27 11:49:05 -0300

    Add client-side code import

--- src/app/components/ReturnButton.tsx
+++ src/app/components/ReturnButton.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { useRouter } from "next/navigation";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
