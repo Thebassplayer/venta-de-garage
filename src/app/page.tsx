2910bd52ad3a1dac11ef63a58ac1f2a96f376050
Roy Lopez <roylopezdev@gmail.com>
2024-02-27 11:13:50 -0300

    Refactor Home component to server component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,28 @@
-"use client";
 import React from "react";
 import Card from "./components/Card";
 import MainNav from "./components/MainNav";
 import Header from "./components/Header";
-import Loading from "./components/Loading";
-import useArticlesData from "./hooks/useArticlesData";
+import { Article } from "./types";
 
 const whatsappMessage = "Hola! estoy interesad@ en este artículo:";
 const whattsappShareMessage =
   "Hola! te comparto este  sitio con artículos a la venta:";
 
-export default function Home() {
-  const { loading, error, tableData } = useArticlesData();
+const localUrl = process.env.LOCAL_URL;
+const articlesEndpoint = process.env.ARTICLES_ENDPOINT;
 
-  if (loading) {
-    return <Loading />;
+async function getData(): Promise<{ tableData: Article[] }> {
+  const res = await fetch(`${localUrl}${articlesEndpoint}`);
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
   }
 
-  if (error) {
-    return (
-      <main className="h-screen w-screen">
-        <div className="flex h-full w-full items-center justify-center">
-          Error de conexion, intentelo mas tarde!
-        </div>
-      </main>
-    );
-  }
+  return res.json();
+}
+
+export default async function Home() {
+  const { tableData } = await getData();
 
   return (
     <main className="relative flex w-full flex-col justify-center p-4">
@@ -33,7 +30,6 @@ export default function Home() {
       <div className="px-3 pb-3 pt-20">
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
           {tableData.map((article, index) => {
-            if (article.vendido === "TRUE") return;
             return <Card key={index} article={article as any} />;
           })}
         </div>
