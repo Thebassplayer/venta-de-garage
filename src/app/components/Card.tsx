3e49b9ed64c969d9aede12081fae72e9b074338f
Roy Lopez <roylopezdev@gmail.com>
2024-02-28 20:46:33 -0300

    Refactor Card component styles

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -20,46 +20,44 @@ const Card = ({ article }: CardProps) => {
 
   return (
     <CardContainer className="inter-var">
-      <CardBody className="group/card relative h-auto  w-auto rounded-xl border border-black/[0.1] bg-gray-50/50 p-6 dark:border-white/[0.2] dark:bg-black dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] sm:w-[30rem]  ">
+      <CardBody className="group/card relative h-auto w-auto rounded-xl border border-black/[0.1] bg-gray-50/50 p-8 dark:border-white/[0.2] dark:bg-black dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] sm:w-[30rem]  ">
         <CardItem
           translateZ="50"
-          className="text-xl font-bold text-neutral-600 dark:text-white"
+          className="text-xl font-bold text-black md:text-3xl"
         >
           {title}
         </CardItem>
         <CardItem
           as="p"
-          translateZ="60"
-          className="mt-2 max-w-sm text-sm text-neutral-500 dark:text-neutral-300"
+          translateZ="70"
+          className="w.full mt-2 line-clamp-4 h-[6rem] text-sm text-neutral-800 dark:text-neutral-300 md:text-base"
         >
           {detail}
         </CardItem>
-        <CardItem translateZ="100" className="mt-4 w-full">
+        <CardItem translateZ="130" className="mt-4 w-full">
           <Image
             src={firstImage}
             height="1000"
             width="1000"
-            className="h-60 w-full rounded-xl object-cover group-hover/card:shadow-xl"
+            className="w-100 mx-auto h-full rounded-xl object-cover"
             alt="thumbnail"
           />
         </CardItem>
-        <div className="mt-20 flex items-center justify-between">
+        <div className="mt-16 flex items-center justify-between">
           <CardItem
-            translateZ={20}
+            translateZ="50"
             as="button"
-            className="rounded-xl px-4 py-2 text-xs font-normal dark:text-white"
+            className="rounded-xl px-4 py-2 text-xs font-bold dark:text-white md:text-3xl"
           >
             {price}
           </CardItem>
-          <Link href={`${baseURL}article/${slug}`}>
-            <CardItem
-              translateZ={20}
-              as="button"
-              className="rounded-xl bg-black px-4 py-2 text-xs font-bold text-white dark:bg-white dark:text-black"
-            >
-              + Info
-            </CardItem>
-          </Link>
+          <CardItem
+            translateZ="50"
+            as="button"
+            className="rounded-xl bg-black px-6 py-3 text-xs font-bold text-white shadow-lg hover:shadow-black dark:bg-white dark:text-black md:text-base"
+          >
+            <Link href={`${baseURL}article/${slug}`}>+ Info</Link>
+          </CardItem>
         </div>
       </CardBody>
     </CardContainer>
